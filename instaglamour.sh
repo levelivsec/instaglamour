97b005832c77b072ef0362319e2f27344958f4b8
levelivsec <67784862+levelivsec@users.noreply.github.com>
2020-07-05 16:02:24 +0200

    Add files via upload

---
+++ instaglamour.sh
@@ -0,0 +1,253 @@
+#!/bin/bash
+# Instaglamour v2.0
+# coded by: @levelivsecurity
+# github.com/levelsec/istaglamour
+# If you use any part from this code, give me the credits, please, read the License
+
+
+string4=$(openssl rand -hex 32 | cut -c 1-4)
+string8=$(openssl rand -hex 32  | cut -c 1-8)
+string12=$(openssl rand -hex 32 | cut -c 1-12)
+string16=$(openssl rand -hex 32 | cut -c 1-16)
+device="android-$string16"
+uuid=$(openssl rand -hex 32 | cut -c 1-32)
+phone="$string8-$string4-$string4-$string4-$string12"
+guid="$string8-$string4-$string4-$string4-$string12"
+header='Connection: "close", "Accept": "*/*", "Content-type": "application/x-www-form-urlencoded; charset=UTF-8", "Cookie2": "$Version=1" "Accept-Language": "en-US", "User-Agent": "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
+var=$(curl -i -s -H "$header" https://i.instagram.com/api/v1/si/fetch_headers/?challenge_type=signup&guid=$uuid > /dev/null)
+var2=$(echo $var | grep -o 'csrftoken=.*' | cut -d ';' -f1 | cut -d '=' -f2)
+ig_sig="4f8732eb9ba7d1c8e8897a75d6474d4eb3f5279137431b2aafb71fafe2abe178"
+
+
+
+banner() {
+                                                         
+printf "\n"
+while read -r row
+do
+    echo -e "\e[1;31m $row"
+done < baner.txt
+printf "\e[1;31m[\e[0m\e[1;77mv2.0\e[0m\e[1;31m]\e[0m\e[1;32m github.com/levelivsec/instaglamour\e[0m\n"
+printf "\e[1;31m[\e[0m\e[1;77m</>\e[0m\e[1;31m]\e[0m\e[1;32mAUTHERS\e[0m\n"
+printf "\e[1;32m>\e[1;31m$\e[1;36mSpecter Traww\e[0m\n"
+printf "\e[1;32m>\e[1;31m$\e[1;36mSnakes 001\e[0m\n"
+printf "\e[1;32m>\e[1;31m$\e[1;36mBlack Ninja\e[0m\n"
+
+}
+
+
+
+login_user() {
+
+
+if [[ $user == "" ]]; then
+read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93mPlease supply your instagram Username: \e[0m' user
+fi
+
+if [[ -e cookie.$user ]]; then
+
+printf "\e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Saved cookies found for user\e[0m\e[1;77m %s\e[0m\n" $user
+
+default_use_cookie="Y"
+
+read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Use it?\e[0m\e[1;77m [Y/n]\e[0m ' use_cookie
+
+use_cookie="${use_cookie:-${default_use_cookie}}"
+
+if [[ $use_cookie == *'Y'* || $use_cookie == *'y'* ]]; then
+printf "\e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Using saved credentials\e[0m\n"
+else
+rm -rf cookie.$user
+login_user
+fi
+
+
+else
+
+read -s -p $'\e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93mPlease supply your instagram Password: \e[0m' pass
+printf "\n"
+data='{"phone_id":"'$phone'", "_csrftoken":"'$var2'", "username":"'$user'", "guid":"'$guid'", "device_id":"'$device'", "password":"'$pass'", "login_attempt_count":"0"}'
+
+IFS=$'\n'
+
+hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
+useragent='User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
+
+printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Trying to login as\e[0m\e[1;93m %s\e[0m\n" $user
+IFS=$'\n'
+var=$(curl -c cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/accounts/login/" | grep -o "logged_in_user\|challenge\|many tries\|Please wait" | uniq ); 
+if [[ $var == "challenge" ]]; then printf "\e[1;93m\n[!] Challenge required\n" ; exit 1; elif [[ $var == "logged_in_user" ]]; then printf "\e[1;92m \n[+] Login Successful\n" ; elif [[ $var == "Please wait" ]]; then echo "Please wait"; fi; 
+
+fi
+
+}
+
+
+geo_media() {
+
+curl -L -b cookie -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/maps/user//"
+
+}
+
+
+follow() {
+
+username_id=$(curl -L -s 'https://www.instagram.com/'$user'' > getid && grep -o  'profilePage_[0-9]*.' getid | cut -d "_" -f2 | tr -d '"')
+
+user_id=$(curl -L -s 'https://www.instagram.com/'$user_account'' > getid && grep -o  'profilePage_[0-9]*.' getid | cut -d "_" -f2 | tr -d '"')
+data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$user_id'", "_csrftoken":"'$var2'"}'
+hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
+curl -L -b cookie -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/create/$user_id/" 
+
+
+}
+
+
+unfollow() {
+
+
+username_id=$(curl -L -s 'https://www.instagram.com/'$user'' > getid && grep -o  'profilePage_[0-9]*.' getid | cut -d "_" -f2 | tr -d '"')
+
+user_id=$(curl -L -s 'https://www.instagram.com/'$user_account'' > getid && grep -o  'profilePage_[0-9]*.' getid | cut -d "_" -f2 | tr -d '"')
+
+data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$user_id'", "_csrftoken":"'$var2'"}'
+hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
+
+check_unfollow=$(curl -L -b cookie -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/destroy/$user_id/" | grep -o '"following": false')
+if [[ $check_unfollow == "" ]]; then
+printf "\e[1;93m [!] Error\n"
+else
+printf "\e[1;92mOK\e[0m\n"
+fi
+}
+
+
+
+increase_followers() {
+
+printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m] This technique consists of following and unfollowing celebgrams, it first follows then unfollows.\e[0m\n"
+printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m] Your followers can be increase up to 20+ in 25 minutues. \e[0m\n"
+printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m]\e[0m\e[1;93m Press Ctrl + C to stop the process. \e[0m\n"
+printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m]\e[0m\e[1;93m Note: you can repeat the process as many times you want. \e[0m\n"
+printf "\e[1;77m[\e[0m\e[1;31m+\e[0m\e[1;77m]\e[0m\e[1;93m     :but don't overdo this because istagram may see it and block your account! \e[0m\n"
+sleep 6
+
+username_id=$(curl -L -s 'https://www.instagram.com/'$user'' > getid && grep -o  'profilePage_[0-9]*.' getid | cut -d "_" -f2 | tr -d '"')
+
+dualipa="12331195"
+mileycyrus="325734299"
+
+madonna="181306552"
+leonardodicaprio="1506607755"
+ladygaga="184692323"
+taylorswift="11830955"
+instagram="25025320"
+shawnmendes="212742998"
+katyperry="407964088"
+charlieputh="7555881"
+lelepons="177402262"
+camila_cabello="19596899"
+
+
+
+cristiano="173560420"
+kimkardashian="18428658"
+kendall="6380930"
+selena="460563723"
+neymar="26669533"
+ariana="7719696"
+kylie="12281817"
+jelopez="305701719"
+messi="427553890"
+beyonce="247944034"
+therock="232192182"
+
+
+
+if [[ ! -e celeb_id ]]; then
+printf "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" $dualipa $mileycyrus $shawnmendes $katyperry $charlieputh $lelepons $camila_cabello $madonna $leonardodicaprio $ladygaga $taylorswift $instagram $neymar $selena $ariana $beyonce $cristiano $kimkardashian $kendall $therock $kylie $jelopez $messi > celeb_id
+fi
+
+while [[ true ]]; do
+
+
+for celeb in $(cat celeb_id); do
+
+data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$celeb'", "_csrftoken":"'$var2'"}'
+hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Following celebgram %s =>" $celeb
+
+check_follow=$(curl -s -L -b cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/create/$celeb/" | grep -o '"following": true')
+
+if [[ $check_follow == "" ]]; then
+printf "\n\e[1;93m [!] Error while trying to follow celebgrams\n"
+printf "\n\e[1;93m [info]If you have once used this technique try not to use saved cookies or try again after 24 hours \n"
+printf "\n\e[1;93m [info]If you wish you can boost use this trick on other accounts \n"
+exit 1
+else
+printf "\e[1;92mDone\e[0m\n"
+fi
+
+sleep 3
+
+done
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping for 60 seconds...\e[0m\n"
+sleep 60
+#unfollow
+for celeb in $(cat celeb_id); do
+data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$celeb'", "_csrftoken":"'$var2'"}'
+hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Unfollowing celebgram %s ..." $celeb
+check_unfollow=$(curl -s -L -b cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/destroy/$celeb/" | grep -o '"following": false' ) 
+
+if [[ $check_unfollow == "" ]]; then
+printf "\n\e[1;93m [!] Error, stoping to prevent your account from bieng blocked\n"
+printf "\n\e[1;93m [info] If you have once used this technique try not to use saved cookies or try again after 24 hours g\n"
+printf "\n\e[1;93m [info]If you wish you can boost use this trick on other accounts\n"
+exit 1
+else
+printf "\e[1;92mOK\e[0m\n"
+fi
+
+sleep 3
+done
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping for 60 seconds...\e[0m\n"
+sleep 60
+
+
+done
+
+
+}
+
+
+friendship() {
+
+
+data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$user_id'", "_csrftoken":"'$var2'"}'
+hmac=$(echo -n "$data" | openssl dgst -sha256 -hmac "${ig_sig}" | cut -d " " -f2)
+curl -L -b cookie.$user -d "ig_sig_key_version=4&signed_body=$hmac.$data" -s --user-agent 'User-Agent: "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"' -w "\n%{http_code}\n" -H "$header" "https://i.instagram.com/api/v1/friendships/show/$user_id/"
+
+}
+
+
+
+
+
+
+menu() {
+
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m$>\e[0m\e[1;31m]\e[0m\e[1;36m INCREASE YOUR INSTAGRAM FOLLOWERS\e[0m\n"
+
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m$>\e[0m\e[1;31m]\e[0m\e[1;36m AUTHENTICATE YOURSELF!\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m$>\e[0m\e[1;31m]\e[0m\e[1;36m Note:Your password may not be visible\n when typing it for security reasons!,\n so make sure you supplie the correct details ohterwise it will not work.\e[0m\n \n"
+
+login_user
+increase_followers
+}
+
+
+banner
+menu
